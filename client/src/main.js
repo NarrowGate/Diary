825d7cf06b1af4e820762820515eeef0bcd0cc16
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2020-11-03 08:23:53 +1000

    Move html  from UI project to client index

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Appsample from './App-.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#elDemo',
+  render: h => h(Appsample)
 })
